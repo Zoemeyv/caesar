3e01547dc16fc2b9995946559fd53721b0d2cb79
zoe meyer <zoe.meyer.vallilengua@gmail.com>
2026-06-13 16:08:34 +0200

    my initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+
+aufg = int(input("Moechtest du eine Nachicht Option 1: verschluesseln ODER Option 2: entschluesseln? SCHREIBE 1 ODER 2! :"))
+
+
+if aufg == 1:
+   msg = input("wie lautet deine nachicht?: ")
+   schluessel = input("wie lautet der schluessel?(zwischen 0 und 25): ")
+   ceasar_code = ""
+   
+   if 0 <= schluessel <= 25 :
+         for char in msg:
+           if char.isalpha():
+              code = ord(char)
+
+              if char.isupper():
+                 code = (code -ord('A') + schluessel) % 26 + ord('A')
+                 ceasar_code += chr(code)
+              elif char.islower():
+                   code = (code -ord('a') + schluessel) % 26 + ord('a')
+                   ceasar_code += chr(code)
+           else:
+                 ceasar_code += char
+         print(f"die verschluesselte Nachicht lautet: {ceasar_code} ")
+   else:
+    print("inkorekter Schluessel!")
+
+      
+elif aufg == 2:
+    msg = input("wie lautet deine geheime! Nachicht? : ")
+    schluessel = input( "wie lautet der schluessel?(zwischen 0 und 25): ")
+    if 0 <= schluessel <= 25:
+        entschluessel = -abs(schluessel)
+        ceasar_code = ""
+
+        for char in msg:
+            if char.isalpha():
+                code = ord(char)
+
+                if char.isupper():
+                    code = (code -ord('A') + entschluessel) % 26 + ord('A')
+                    ceasar_code += chr(code)
+                elif char.islower():
+                     code = (code -ord('a') + entschluessel) % 26 + ord('a')
+                     ceasar_code += chr(code)
+            else:
+                   ceasar_code += char
+
+        print(f"die verschluesselte Nachicht lautet: {ceasar_code} ")
+
+    else:
+        print("inkorekter Schluessel!")
+            
+else:
+    print("error: Achte darauf es genau so zu schreiben wie es da steht!")
